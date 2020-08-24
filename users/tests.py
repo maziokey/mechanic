f31aa2eb35638d5b6f21b1e677283bf598beef60
maziokey <okeyluvsu2004@yahoo.com>
2020-08-24 22:15:44 +0100

    Login, logout & signup functionality added

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-#from django.urls import reverse, resolve
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm # new
+from .views import SignupPageView # new
 
 
 # Create your tests here.
@@ -31,3 +34,27 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+class SignupTests(TestCase):
+
+    #username = 'newuser'
+    #email = 'newuser@email.com'
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there, who u be.')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
